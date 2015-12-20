3730f42c5ed0de132f268030fb4e2fbc3b15a432
WesTyler <westyler5@gmail.com>
2015-12-20 13:32:50 -0600

    Add About Us copy and styling

--- client/app.js
+++ client/app.js
@@ -5,18 +5,20 @@ BlueWater.controller = function() {};
 BlueWater.view = function(ctrl) {
   return m('.app', [
     m('.header', [
-      m('picture', [
+      m('picture', {class: 'header-image clearfix'}, [
         m('source', {media: '(min-width: 650px)', srcset: 'assets/large-header.png'}),
         m('source', {media: '(min-width: 465px)', srcset: 'assets/medium-header.png'}),
         m('img', {src: 'assets/small-header.png', alt: 'BlueWater Energy Services, LLC'})
       ])
     ]),
     m('nav.clearfix', [
+      m('hr'),
       m('ul', [
         m('li', [m('a',{class: 'navLink homeNav', href: '/'}, 'Home')]),
         m('li', [m('a', {class: 'navLink servicesNav', href: '#/services'}, 'About Our Services')]),
         m('li', [m('a',{class: 'navLink aboutNav', href: '#/about'}, 'About Us')])
-      ])
+      ]),
+      m('hr')
     ])
   ]);
 };
