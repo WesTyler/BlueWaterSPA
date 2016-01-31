33d82b4b6e38a9154e3596ea5a5bb92f6a5192de
WesTyler <westyler5@gmail.com>
2016-01-30 21:01:00 -0600

    Fill in Home page details.

--- client/components/home.js
+++ client/components/home.js
@@ -6,6 +6,8 @@ Home.controller = function() {
 
 Home.view = function(ctrl) {
   return m('div', {class: 'homepage clearfix'}, [
-      m('div', 'This should be the homepage content.')
+      m('div', 'Welcome to BlueWater Energy Services, LLC. Thank you for visiting us. For information about who we are and what we do, please see our About pages.'),
+      m('div', 'To request pricing or more detailed information, please visit our Request A Quote page.'),
+      m('div', 'To contact us, please see our details at the bottom of all pages.')
   ]);
 };
