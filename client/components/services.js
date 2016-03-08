224768fa42b8f697a036e3b879fb384635bdfc14
WesTyler <westyler5@gmail.com>
2016-03-07 23:02:33 -0600

    Add product details for drive over and polyurea.

--- client/components/services.js
+++ client/components/services.js
@@ -34,7 +34,16 @@ Services.view = function(ctrl){
       m('h4.aboutItem', {onclick: function() {ctrl.toggleService('muscleWall')}}, 'Muscle Wall Secondary Containment', [
         function() {
           if (ctrl.selectedService() === 'muscleWall') {
-            return m('.details', 'These are the details about Muscle Wall Secondary Containment');
+            return m('.details', [
+              m('h4', 'Time is money and Muscle Wall saves both of those.'),
+              m('div', 'Muscle Wall was designed so that it can be quickly deployed.  Time is money for our clients so we want to be able to provide them with the most reliable product on the market that can be set up and taken down in as little time as possible.'),
+              m('br'),
+              m('div', 'The lightweight of each wall coupled with the intuitive design allows Muscle Wall to be deployed and taken down with just a small team of workers and without the aid of heavy equipment.  A 2ft secondary containment big enough to house a standard frac tank can be set up in just 30 minutes.'),
+              m('br'),
+              m('br'),
+              m('h4', 'Keep the environment in mind with Muscle Wall.'),
+              m('div', 'Muscle Wall was designed with the environment in mind.  By using Muscle Wall as secondary containment, negative impacts to the environment, such as erosion or soil and water contamination, are greatly reduced.')
+            ]);
           }
         }()
       ]),
@@ -48,7 +57,7 @@ Services.view = function(ctrl){
       m('h4.aboutItem', {onclick: function() {ctrl.toggleService('polyUrea')}}, 'Polyurea Sprayed Secondary Containment', [
         function() {
           if (ctrl.selectedService() === 'polyUrea') {
-            return m('.details', 'These are the details about Polyurea Sprayed Secondary Containment');
+            return m('.details', 'Sprayed on polyurea offers a superior containment solution for all containment applications.  It can be custom build into any size facility or jobsite.  Polyurea\'s chemical resistance properties, tensile strength and durability surpass other liner options.');
           }
         }()
       ])
