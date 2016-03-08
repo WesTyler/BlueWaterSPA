63b9626dd681ffe8225136220766f8a0889330ba
Wes Tyler <westyler5@gmail.com>
2016-03-08 13:10:42 -0600

    Update quote form and vm

--- client/components/quote.js
+++ client/components/quote.js
@@ -1,24 +1,26 @@
 var QuotePage = exports = {};
 
 QuotePage.view = function(ctrl) {
-  return m('div', [m('form', {class: 'quoteForm clearfix'}, [
-      m('label', 'Contact Name', [
-        m('input', {value: QuotePage.vm.name(), oninput: m.withAttr('value', QuotePage.vm.name)})
-      ]),
-      m('label', 'Company', [
-        m('input', {value: QuotePage.vm.companyName(), oninput: m.withAttr('value', QuotePage.vm.name)})
-      ]),
-      m('input', {value: QuotePage.vm.email(), oninput: m.withAttr('value', QuotePage.vm.email)}),
-      m('input', {value: QuotePage.vm.phone(), oninput: m.withAttr('value', QuotePage.vm.phone)}),
-      m('select', {onchange: function(e) {ctrl.toggleService(e)}}, [
-        m('option', 'Liner 1'),
-        m('option', 'Liner 2'),
-        m('option', 'Liner 3')
-      ]),
-      m('input', {value: QuotePage.vm.comments(), oninput: m.withAttr('value', QuotePage.vm.comments)}),
-      m('button', {onclick: QuotePage.vm.save}, 'Save Changes')
-  ]),
-  m('div', 'Values \n' , QuotePage.vm.name(), '\n', QuotePage.vm.email(), '\n', QuotePage.vm.phone(), QuotePage.vm.comments())]);
+  return m('div', [
+      m('form', {class: 'quoteForm clearfix'}, [
+          m('label', {class: 'quoteItem'}, 'Contact Name', [
+            m('input', {value: QuotePage.vm.name(), oninput: m.withAttr('value', QuotePage.vm.name)})
+          ]),
+          m('label', {class: 'quoteItem'}, 'Company', [
+              m('input', {value: QuotePage.vm.companyName(), oninput: m.withAttr('value', QuotePage.vm.companyName)})
+          ]),
+          m('label', {class: 'quoteItem'}, 'Email', [
+              m('input', {value: QuotePage.vm.email(), type: 'email', oninput: m.withAttr('value', QuotePage.vm.email)})
+          ]),
+          m('label', {class: 'quoteItem'}, 'Phone', [
+              m('input', {value: QuotePage.vm.phone(), type: 'tel', oninput: m.withAttr('value', QuotePage.vm.phone)})
+          ]),
+          m('label', {class: 'quoteItem'}, 'Other Comments', [
+              m('input', {value: QuotePage.vm.comments(), oninput: m.withAttr('value', QuotePage.vm.comments)})
+          ]),
+          m('button', {class: 'quoteItem', onclick: QuotePage.vm.save}, 'Save Changes')
+      ])
+  ])
 };
 
 QuotePage.vm = {
@@ -27,27 +29,27 @@ QuotePage.vm = {
     QuotePage.vm.companyName = m.prop('');
     QuotePage.vm.email = m.prop('');
     QuotePage.vm.phone = m.prop('');
-    QuotePage.vm.services = m.prop('');
     QuotePage.vm.comments = m.prop('');
 
     QuotePage.vm.quoteData = m.prop({});
 
     QuotePage.vm.save = function() {
+        //console.log('CALLING QuotePAge.vm.save')
       var quoteData = {
         name    : QuotePage.vm.name(),
+        company : QuotePage.vm.companyName(),
         email   : QuotePage.vm.email(),
         phone   : QuotePage.vm.phone(),
-        services: QuotePage.vm.services(),
         comments: QuotePage.vm.comments()
       };
 
       QuotePage.vm.quoteData(new Quote(quoteData));
       console.log('Quote model: \n', QuotePage.vm.quoteData())
-      QuotePage.vm.name = m.prop('');
-      QuotePage.vm.email = m.prop('');
-      QuotePage.vm.phone = m.prop('');
-      QuotePage.vm.services = m.prop([]);
-      QuotePage.vm.comments = m.prop('');
+      QuotePage.vm.name('');
+      QuotePage.vm.companyName('');
+      QuotePage.vm.email('');
+      QuotePage.vm.phone('');
+      QuotePage.vm.comments('');
     };
   }
 };
